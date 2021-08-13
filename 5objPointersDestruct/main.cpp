eceda6e8e03e013c79168f0aa569a63381a4a4df
NiklasStrothoff <strothoffniklas@gmail.com>
2021-08-13 17:12:29 +0200

    h

--- 5objPointersDestruct/main.cpp
+++ 5objPointersDestruct/main.cpp
@@ -9,4 +9,20 @@ int main () {
     WARRIOR playerW("nik", 21, "German");
     playerW.showData();
     playerW.showWeapons();
+
+    //FOR MANY OBJ THAT NEED TO BE ALLOCATED DYNAMICALLY USE POINTER TO OBJ INSTEAD OF CREATING OBJ ITSELF
+    WARRIOR warr1("idk", 33, "natio"); //OBJ CREATED WITH AUTOMATIC STORAGE DURATION: DESTROYED WHEN OUT OF SCOPE
+    new WARRIOR("nin", 23, "ad");//OBJ CREATED WITH DYNAMIC STORAGE DURATION: DESTROYED ONLY WHEN CALLING delete: AVOID WHEN POSSIBLE AND HAVE TO BE USED WITH SMART POINTERS
+    
+    //CREATE POINTERS FOR OBJ TO NULL
+    WARRIOR *pWarrior2 = nullptr;
+    PLAYER *pPlayer2 = nullptr;
+    //CREATE OBJA AND SAVE IT INTO POINTER
+    pPlayer2 = new PLAYER("idffk", 33, "nafftio");
+    //CALL FUNCTION FOR PPLAYER2
+    (*pPlayer2).showData();
+
+    //MORE DOWN
+    pWarrior2 = new WARRIOR("idaak", 33, "naaatio");
+    pWarrior2->showData(); //ALTERNATIVE WAY TO ACCESS OBJ METHODS FORM A POINTER
 }
